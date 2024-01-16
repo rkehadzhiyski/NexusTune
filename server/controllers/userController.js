fbced1040efc61d3a1667a3b9ea0b5ae5c1bd50f
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-16 21:15:14 +0200

    create login manager

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,15 +7,30 @@ router.post('/register', async (req, res) => {
         const user = await userManager.rregister(req.body);
 
         res.json({
-            authToken: '',
-            email: user.email,
             userId: user._id,
+            email: user.email,
+            accessToken: 'no-token',
         })
     } catch (error) {
         res.status(400).json({
-            message: 'There was an error',
+            message: 'There was an error:',
+            message: err.message,
         });
         console.log(error);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const result = await userManager.login(req.body);
+
+        res.json(result);
+    } catch (error) {
+        res.status(400).json({
+            message: 'There was an error:',
+            message: err.message,
+        });
+        
     }
 });
 
--- server/managers/userManager.js
+++ server/managers/userManager.js
@@ -1,3 +1,34 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.register = (userData) => User.create(userData);
+
+exports.login = async ({ email, password }) => {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new Error('Invalid username or password');
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw new Error('Invalid username or password');
+    }
+
+    const payload = {
+        userId: user._id,
+        email: user.email,
+    };
+    //TODO: Make it async
+    const token = jwt.sign(payload, 'SECRETSECRET', { expiresIn: '1d' });
+
+    const result = {
+        userId: user._id,
+        email: user.email,
+        accessToken: token,
+    };
+
+    return result;
+};

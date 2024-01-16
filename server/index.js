53be26b47e2f7aa8bd583ac37382a53ba98e9aad
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-16 20:26:50 +0200

    url and json parser middleware

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const routes = require('./routes');
 
 const app = express();
 
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('RESTful service');
 });

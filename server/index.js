277f2d2c5a83d755096ade484adebfe44fa9c759
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-16 20:59:08 +0200

    connect db

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const routes = require('./routes');
 
 const app = express();
 
-app.use(express.urlencoded({extended:false}));
+mongoose.connect('mongodb://127.0.0.1:27017/nexustune')
+    .then(() => console.log('DB connected'))
+    .catch(error => console.log(error));
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 

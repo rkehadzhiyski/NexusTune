d4b773e0f4130371ad6e6401683a30aa0b937560
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-16 15:32:48 +0200

    userController

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
+const routes = require('./routes');
+
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.send('RESTful service');
 });
+
+app.use(routes);
 
 app.listen(8888, () => console.log('Server is listening on port 8888'));

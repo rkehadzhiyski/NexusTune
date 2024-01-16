1399a9e4f500203eaaa9fa4f609d3e67098630f6
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-16 20:56:43 +0200

    add register to user controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
 
-router.post('/register', (req, res) => {
+const userManager = require('../managers/userManager');
 
+router.post('/register', async (req, res) => {
+    try {
+        const user = await userManager.rregister(req.body);
+
+        res.json({
+            authToken: '',
+            email: user.email,
+            userId: user._id,
+        })
+    } catch (error) {
+        res.status(400).json({
+            message: 'There was an error',
+        });
+        console.log(error);
+    }
 });
 
 module.exports = router;

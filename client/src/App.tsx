9193350e62b7a476d62a1fa682b5851e390dacc2
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-17 12:01:25 +0200

    add home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import Navigation from "./components/navigation/Navigation";
 import Register from "./components/register/Register";
+import Home from "./components/home/Home";
 
 const App = () => {
     return (
         <>
             <Navigation />
-            <h1>Hello HOME</h1>
             <Routes>
+                <Route path="/" element={<Home/>} ></Route>
                 <Route path="/login" ></Route>
                 <Route path="/register" element={<Register />} ></Route>
             </Routes>

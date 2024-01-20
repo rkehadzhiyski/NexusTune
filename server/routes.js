a308a8a93cd8ab2cfe7c98095572ca2e54caa11f
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-20 17:13:24 +0200

    add podcast controller to the router

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const userController = require('./controllers/userController');
+const podcastContoller = require('./controllers/podcastController');
 
 router.use('/users', userController);
+router.use('/podcasts', podcastContoller);
 
 module.exports = router;

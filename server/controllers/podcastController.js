08484d3051449bd9ac4075b064f707b4e97c8131
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-21 20:57:24 +0200

    add delete to controller

--- server/controllers/podcastController.js
+++ server/controllers/podcastController.js
@@ -22,14 +22,20 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.get('/:podcastId', async (req,res) =>{
+router.get('/:podcastId', async (req, res) => {
     const podcast = await podcastManager.getOne(req.params.podcastId);
 
     res.json(podcast);
 });
 
-router.put('/:podcastId', async (req,res) => {
+router.put('/:podcastId', async (req, res) => {
     await podcastManager.update(req.params.podcastId, req.body);
+
+    res.status(204).end();
+});
+
+router.delete('/:podcastId', async (req, res) => {
+    await podcastManager.delete(podcastId);
 
     res.status(204).end();
 });

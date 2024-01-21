027f8b58f734ae426818d6cd04df79c256ec590f
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-21 20:48:36 +0200

    add get one in the manager

--- server/managers/podcastManager.js
+++ server/managers/podcastManager.js
@@ -2,4 +2,6 @@ const Podcast = require('../models/Podcast');
 
 exports.getAll = () => Podcast.find();
 
+exports.getOne = (podcastId) => Podcast.findById(podcastId);
+
 exports.create = (podcastData) => Podcast.create(podcastData);

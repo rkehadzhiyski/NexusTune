85f27a0610fa4894a3a442a00f78b2fe92fe5d82
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-22 14:04:39 +0200

    upload to storage and get url

--- client/src/components/upload/Upload.tsx
+++ client/src/components/upload/Upload.tsx
@@ -1,17 +1,33 @@
 import { ChangeEvent, useState } from "react";
 import { storage } from '../../../firebase';
-import { ref, uploadBytes } from 'firebase/storage';
+import { getDownloadURL, list, ref, uploadBytes } from 'firebase/storage';
 
 const Upload = () => {
     const [audioUpload, setAudioUpload] = useState<File>();
-    const uploadImage = () => {
-        if (audioUpload == null) return;
-        const audioRef = ref(storage, `audio/${audioUpload.name}`);
-        
-        uploadBytes(audioRef, audioUpload).then(() => {
+    const [audioUrl, setAudioUrl] = useState<string | null>();
 
-        });
+    const uploadImage = async () => {
+        if (audioUpload == null) return;
+
+        const folderRef = ref(storage, 'audio/');
+        const audioRef = ref(storage, `audio/${audioUpload.name}`);
+
+        try {
+            await uploadBytes(audioRef, audioUpload);
+
+            const response = await list(folderRef);
+
+            response.items.map(async (item) => {
+                const url = await getDownloadURL(item);
+                setAudioUrl(url);
+            });
+        } catch (error) {
+            console.error("Error uploading or fetching audio:", error);
+        }
     };
+    
+    //TODO : send the url to the database
+    console.log(audioUrl)
 
     const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
         if (event.target.files && event.target.files.length > 0) {

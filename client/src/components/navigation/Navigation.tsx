f8d1a6407603f5338d14cc27cc8e4a4b06574e1b
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-25 12:19:34 +0200

    add more links to nav dropdown

--- client/src/components/navigation/Navigation.tsx
+++ client/src/components/navigation/Navigation.tsx
@@ -18,17 +18,18 @@ const Navigation = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand as={Link} to="/">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/">Nexus Tune</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link as={Link} to="/create-podcast">Upload</Nav.Link>
                     </Nav>
                     <Nav>
                         {isAuthenticated && (
                             <div className={styles['profile-info-container']}>
                                 <img className={styles['profile-image']} src="/profile-image.jpg" alt="profile-image" />
                                 <NavDropdown title={user.username} id="nav-dropdown">
+                                    <NavDropdown.Item as={Link} to="/my-profile">My Profile</NavDropdown.Item>
+                                    <NavDropdown.Item as={Link} to="/create-podcast">Add Podcast</NavDropdown.Item> 
                                     <NavDropdown.Divider />
                                     <NavDropdown.Item as={Link} onClick={logoutHandler} to="/">Logout</NavDropdown.Item>
                                 </NavDropdown>

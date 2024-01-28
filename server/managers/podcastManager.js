cd6693a463345bfbe3208c21c47633910b414675
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-28 10:57:36 +0200

    add get all of owner to manager

--- server/managers/podcastManager.js
+++ server/managers/podcastManager.js
@@ -6,6 +6,8 @@ exports.getOne = (podcastId) => Podcast.findById(podcastId);
 
 exports.getAll = () => Podcast.find();
 
+exports.getAllOfOwner = (ownerId) => Podcast.find(ownerId);
+
 exports.update = (podcastId) => Podcast.findByIdAndUpdate(podcastId);
 
 exports.delete = (podcastId) => Podcast.findByIdAndDelete(podcastId);

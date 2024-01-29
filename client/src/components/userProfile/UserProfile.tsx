90af239f994fdd9551adf95e153c5636b6e12ad3
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-29 11:43:32 +0200

    add button for profile edit

--- client/src/components/userProfile/UserProfile.tsx
+++ client/src/components/userProfile/UserProfile.tsx
@@ -1,13 +1,20 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import AuthContext from '../../contexts/authContext';
 
 import styles from './userProfile.module.css';
 import UserPodcasts from '../userPodcasts/UserPodcasts';
+import Button from 'react-bootstrap/Button';
 
 const UserProfile = () => {
     const {
         user
     } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const navigateTo = () => {
+        navigate('/edit-profile')
+    }
 
     return (
         <div className={styles['profile-page']}>
@@ -20,6 +27,9 @@ const UserProfile = () => {
                         totam maxime hic voluptates
                         sunt dolorum dolore?
                     </p>
+                    <Button onClick={navigateTo} variant="primary" >
+                        Edit Profile
+                    </Button>
                 </div>
             </div>
             <div className={styles['user-podcasts']}>

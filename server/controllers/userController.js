226e5183a3be2ea69258f334286b52995918ad61
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-29 12:59:35 +0200

    add edit to user controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -14,7 +14,7 @@ router.post('/register', async (req, res) => {
             message: 'There was an error:',
             message: error.message,
         });
-        
+
     }
 });
 
@@ -32,8 +32,22 @@ router.post('/login', async (req, res) => {
     }
 });
 
-router.get('/logout', (req,res) => {
+router.get('/logout', (req, res) => {
     res.end();
+});
+
+router.post('/edit/:userId', async (req, res) => {
+    const userId = req.params.userId;
+
+    try {
+        const result = await userManager.edit(userId, req.body);
+        return result;
+    } catch (error) {
+        res.status(400).json({
+            message: 'There was an error:',
+            message: error.message,
+        });
+    }
 });
 
 module.exports = router;

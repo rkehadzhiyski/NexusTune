edfce24ff498b903b6d823392ac1ad3407f54c97
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-29 15:00:01 +0200

    fix user profile

--- client/src/components/userProfile/UserProfile.tsx
+++ client/src/components/userProfile/UserProfile.tsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from 'react';
 import AuthContext from '../../contexts/authContext';
 
-import * as podcastService from '../../services/podcastService';
+import * as userService from '../../services/userService';
 
 import styles from './userProfile.module.css';
 import UserPodcasts from '../userPodcasts/UserPodcasts';
@@ -17,32 +17,29 @@ interface Podcast {
 
 const UserProfile = () => {
     const [modalShow, setModalShow] = useState(false);
-    const [podcast, setPodcast] = useState<Podcast[]>([]);
+    const [userData, setUserData] = useState<Podcast>();
     const {
-        user
+        user,
     } = useContext(AuthContext);
 
     useEffect(() => {
-        podcastService.getOne(user.userId)
+        userService.getOne(user.userId)
             .then(response => {
-                setPodcast(response.data);
+                setUserData(response.data);
             })
             .catch(error => {
                 console.error("Error fetching podcast:", error);
             });
-    },[podcast, user]);
+            console.log(userData)
+    }, [userData, user]);
 
     return (
         <div className={styles['profile-page']}>
             <div className={styles['profile-info-container']}>
-                <img className={styles['profile-picture']} src='\user-image-default.webp' alt='profile-image' />
+                <img className={styles['profile-picture']} src={userData?.image?? '/user-image-default.webp'} alt='profile-image' />
                 <p className={styles['username']}>{user.username}</p>
                 <div className={styles['profile-description']}>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit sint alias perferendis quis
-                        totam maxime hic voluptates
-                        sunt dolorum dolore?
-                    </p>
+                    <p>{userData?.description}</p>
                     <Button onClick={() => setModalShow(true)} variant="primary" >
                         Edit Profile
                     </Button>

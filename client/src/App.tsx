7cbd48ea63a4271ced5661fc157ea21f3c9ff831
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-29 11:46:44 +0200

    add edit profile component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,8 @@ import Register from "./components/register/Register";
 import Home from "./components/home/Home";
 import Login from "./components/login/Login";
 import CreatePodcast from "./components/createPodcast/CreatePodcast";
-import UserProfile from "./components/userProfile/userProfile";
+import EditProfile from "./components/editProfile/EditProfile";
+import UserProfile from "./components/userProfile/UserProfile";
 
 const App = () => {
     return (
@@ -19,6 +20,7 @@ const App = () => {
                     <Route path="/login" element={<Login />}></Route>
                     <Route path="/register" element={<Register />} ></Route>
                     <Route path="/user-profile" element={<UserProfile />}></Route>
+                    <Route path="/edit-profile" element={<EditProfile />} ></Route>                
                     <Route path="/create-podcast" element={<CreatePodcast />} ></Route>
                 </Routes>
                 </div>

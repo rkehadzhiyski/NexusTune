ad6d67601eea83fcb99981caaa23f48206d3e1f3
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-31 20:39:32 +0200

    add loading spinner

--- client/src/components/createEpisode/CreateEpisode.tsx
+++ client/src/components/createEpisode/CreateEpisode.tsx
@@ -7,6 +7,7 @@ import * as userService from '../../services/userService';
 import styles from './createEpisode.module.css';
 
 import { Button, FloatingLabel, Form } from "react-bootstrap";
+import { Spinner } from 'react-bootstrap';
 import AuthContext from '../../contexts/authContext';
 
 interface PodcastData {
@@ -63,12 +64,14 @@ const CreateEpisode = () => {
     const { register, handleSubmit, formState: { errors } } = useForm({
         resolver: yupResolver(schema),
     });
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         userService.getUploadedPodcast(user.userId)
             .then(response => {
                 console.log(response.data);
                 setPodcasts(response.data);
+                setIsLoading(false);
             })
             .catch(error => {
                 console.error('Error fetching podcasts:', error);
@@ -80,86 +83,100 @@ const CreateEpisode = () => {
     }
 
     return (
-        <>
-            {podcasts.length == 0
-                ?
+        <div className={styles['spinner-container']}>
+            {isLoading ? (
+                <Spinner animation="grow" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            ) : (
                 <>
-                    <h1>No Podcasts yet!</h1>
-                </>
-                :
-                <div className={styles['create-podcast-page']}>
-                    <div className={styles['podcast-info-container']}>
-                        <h2>Information</h2>
-                        <p className={styles['info-text']}>Please provide a name, an image, and a brief description for your episode.
-                            This information will be displayed to listeners in the episode directory.
-                            Make sure your image is clear and represents your episode well,
-                            as it will be the first thing potential listeners see.
-                        </p>
-                    </div>
-
-                    <div className={styles['create-podcast-form-container']}>
-                        <Form className={styles['form']} onSubmit={handleSubmit(onSubmit)}>
-                            <h1 className={styles['heading']}>Create Episode</h1>
-                            <FloatingLabel label="Name" className="mb-3" controlId="formGroupName">
-                                <Form.Control
-                                    type="name"
-                                    {...register('name')}
-                                    placeholder="Enter a name"
-                                    autoComplete="name-input"
-                                />
-                                <Form.Text className="text-danger">{errors['name']?.message}</Form.Text>
-                            </FloatingLabel>
-                            <Form.Text className="text-muted">
-                            </Form.Text>
-                            <Form.Label className={styles['image-label']}>Episode image</Form.Label>
-                            <Form.Group controlId="formFileImage" className="mb-3">
-                                <Form.Control
-                                    type="file"
-                                    {...register('episodeImage')}
-                                    // onInput={handleFileChange}
-                                    multiple={false}
-                                    placeholder="episodeImage" />
-                                <Form.Text className="text-danger">{errors['episodeImage']?.message}</Form.Text>
-                            </Form.Group>
-                            <Form.Label className={styles['image-label']}>Episode audio</Form.Label>
-                            <Form.Group controlId="formFileAudio" className="mb-3">
-                                <Form.Control
-                                    type="file"
-                                    {...register('episodeAudio')}
-                                    // onInput={handleFileChange}
-                                    multiple={false}
-                                    placeholder="episodeAudio" />
-                                <Form.Text className="text-danger">{errors['episodeAudio']?.message}</Form.Text>
-                            </Form.Group>
-                            <Form.Select className='mb-3' aria-label="Default select example" {...register('selectedPodcast')}>
-                                <option>Select Podcast</option>
-                                {podcasts.map(podcast => (
-                                    <option key={podcast._id} value={podcast._id}>{podcast.name}</option>
-                                ))}
-                            </Form.Select>
-                            <Form.Text className="text-danger">{errors['selectedPodcast']?.message}</Form.Text>
-                            <FloatingLabel className='mb-3' label="Description" controlId="formGroupDescription">
-                                <Form.Control
-                                    style={{ height: '150px' }}
-                                    as={'textarea'}
-                                    type="text"
-                                    {...register('description')}
-                                    placeholder="Description"
-                                    autoComplete="description"
-                                />
-                                <Form.Text className="text-danger">{errors['description']?.message}</Form.Text>
-                            </FloatingLabel>
-                            <div>
-                                <Button className={styles['create-podcast-button']} variant="primary" type="submit">
-                                    Create
-                                </Button>
+                    {podcasts.length == 0
+                        ?
+                        <>
+                            <h1>No Podcasts yet!</h1>
+                        </>
+                        :
+                        <div className={styles['create-podcast-page']}>
+                            <div className={styles['podcast-info-container']}>
+                                <h2>Information</h2>
+                                <p className={styles['info-text']}>Please provide a name, an image, and a brief description for your episode.
+                                    This information will be displayed to listeners in the episode directory.
+                                    Make sure your image is clear and represents your episode well,
+                                    as it will be the first thing potential listeners see.
+                                </p>
                             </div>
-                        </Form>
-                    </div>
-                </div >
-            }
-        </>
-    );
-}
+
+                            <div className={styles['create-podcast-form-container']}>
+                                <Form className={styles['form']} onSubmit={handleSubmit(onSubmit)}>
+                                    <h1 className={styles['heading']}>Create Episode</h1>
+                                    <FloatingLabel label="Name" className="mb-3" controlId="formGroupName">
+                                        <Form.Control
+                                            type="name"
+                                            {...register('name')}
+                                            placeholder="Enter a name"
+                                            autoComplete="name-input"
+                                        />
+                                        <Form.Text className="text-danger">{errors['name']?.message}</Form.Text>
+                                    </FloatingLabel>
+                                    <Form.Text className="text-muted">
+                                    </Form.Text>
+                                    <div style={{ display: 'flex' }}>
+                                        <div>
+                                            <Form.Label className={styles['image-label']}>Episode image</Form.Label>
+                                            <Form.Group controlId="formFileImage" className="mb-3">
+                                                <Form.Control
+                                                    type="file"
+                                                    {...register('episodeImage')}
+                                                    // onInput={handleFileChange}
+                                                    multiple={false}
+                                                    placeholder="episodeImage" />
+                                                <Form.Text className="text-danger">{errors['episodeImage']?.message}</Form.Text>
+                                            </Form.Group>
+                                        </div>
+                                        <div>
+                                            <Form.Label className={styles['image-label']}>Episode audio</Form.Label>
+                                            <Form.Group controlId="formFileAudio" className="mb-3">
+                                                <Form.Control
+                                                    type="file"
+                                                    {...register('episodeAudio')}
+                                                    // onInput={handleFileChange}
+                                                    multiple={false}
+                                                    placeholder="episodeAudio" />
+                                                <Form.Text className="text-danger">{errors['episodeAudio']?.message}</Form.Text>
+                                            </Form.Group>
+                                        </div>
+                                    </div>
+                                    <Form.Select className='mb-3' aria-label="Default select example" {...register('selectedPodcast')}>
+                                        <option>Select Podcast</option>
+                                        {podcasts.map(podcast => (
+                                            <option key={podcast._id} value={podcast._id}>{podcast.name}</option>
+                                        ))}
+                                    </Form.Select>
+                                    <Form.Text className="text-danger">{errors['selectedPodcast']?.message}</Form.Text>
+                                    <FloatingLabel className='mb-3' label="Description" controlId="formGroupDescription">
+                                        <Form.Control
+                                            style={{ height: '150px' }}
+                                            as={'textarea'}
+                                            type="text"
+                                            {...register('description')}
+                                            placeholder="Description"
+                                            autoComplete="description"
+                                        />
+                                        <Form.Text className="text-danger">{errors['description']?.message}</Form.Text>
+                                    </FloatingLabel>
+                                    <div>
+                                        <Button className={styles['create-podcast-button']} variant="primary" type="submit">
+                                            Create
+                                        </Button>
+                                    </div>
+                                </Form>
+                            </div>
+                        </div >
+                    }
+                </>
+            )}
+        </div>
+    )
+};
 
 export default CreateEpisode;

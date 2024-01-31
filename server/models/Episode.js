677ae27a6ccea1522a59e68ca68e531c58ceb854
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-31 21:14:01 +0200

    create Episode model

---
+++ server/models/Episode.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const episodeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    audio: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        required: true
+    },
+    podcastId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Podcast',
+    },
+    ownerId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    },
+});
+
+const Episode = mongoose.model('Episode', episodeSchema);
+
+module.exports = Episode;

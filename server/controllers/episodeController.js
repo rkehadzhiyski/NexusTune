899613bb1d0bb50b329325a2a6fe169c743b57af
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-01-31 21:19:08 +0200

    add create to episode controller

---
+++ server/controllers/episodeController.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+const episodeManager = require('../managers/episodeManager');
+
+router.post('/', async(req, res) => {
+    try {
+        const episode = await episodeManager.create(req.body);
+        res.json(episode);
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({
+            message: 'Cannot create Episode'
+        });
+    }
+});

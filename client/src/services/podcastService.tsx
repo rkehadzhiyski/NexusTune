6fadf57ec8c51c64b259b2dca6b3ac6d22169f79
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-02-01 11:06:37 +0200

    add edit podcast to podcast service

--- client/src/services/podcastService.tsx
+++ client/src/services/podcastService.tsx
@@ -9,6 +9,10 @@ interface uploadData {
     createdAt: string
 }
 
+interface updateEpisode {
+    episode: string;
+}
+
 export const create = async (data: uploadData) => {
     const result = await axios.post(baseUrl, data);
     return result;
@@ -28,3 +32,8 @@ export const getAllOfOwner = async (ownerId: string) => {
     const result = await axios.get(`${baseUrl}/owner/${ownerId}`);
     return result;
 }
+
+export const editPodcast = async (podcastId: string, updatedData: updateEpisode) => {
+    const result = await axios.post(`${baseUrl}/edit/${podcastId}`, updatedData);
+    return result;
+} 
--- server/models/Podcast.js
+++ server/models/Podcast.js
@@ -17,6 +17,10 @@ const podcastSchema = new mongoose.Schema({
         type: Date,
         required: true
     },
+    episodes: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Episode',
+    },
     ownerId: {
         type: mongoose.Types.ObjectId,
         ref: 'User',

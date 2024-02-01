6a5696c7d2f0173cf94dbb434a112e1061aea5b0
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-02-01 11:29:42 +0200

    add getEpisodes function to the manager

--- server/managers/podcastManager.js
+++ server/managers/podcastManager.js
@@ -8,6 +8,18 @@ exports.getAll = () => Podcast.find();
 
 exports.getAllOfOwner = (ownerId) => Podcast.find({ ownerId: ownerId });
 
+exports.getEpisodes = async(podcastId)  => {
+    try {
+        const podcast = await Podcast.findById(podcastId).populate('episodes');
+        if (!episodes) {
+            throw new Error('No episodes found');
+        }
+        return podcast.episodes;
+    } catch (error) {
+        throw new Error('Error fetching uploaded podcasts');
+    }
+}
+
 exports.update = (podcastId, data) => Podcast.findByIdAndUpdate(podcastId, data);
 
 exports.delete = (podcastId) => Podcast.findByIdAndDelete(podcastId);

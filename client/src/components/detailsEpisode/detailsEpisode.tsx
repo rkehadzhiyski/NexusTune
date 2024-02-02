f479e25805f16beab9e2e1649a20254a4814d13b
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-02-02 22:01:51 +0200

    add some basic html to details episode

--- client/src/components/detailsEpisode/detailsEpisode.tsx
+++ client/src/components/detailsEpisode/detailsEpisode.tsx
@@ -1,10 +1,40 @@
-const detailsEpisode = () => {
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import * as episodeService from '../../services/episodeService';
+
+interface Episode {
+    _id: number;
+    name: string;
+    description: string;
+    image: string;
+    audio: string;
+    createdAt: string
+}
+
+const DetailsEpisode = () => {
+    const [episode,setEpisode] = useState<Episode>();
+    const params = useParams();
+    const { episodeId } = params;
     
+    useEffect(()=>{
+        if (episodeId) {            
+            episodeService.getOne(episodeId)
+                .then(response => {
+                    setEpisode(response.data)
+                }).catch(error => {
+                    console.error('Error fetching episodes:', error);
+                });
+        }
+    },[episodeId])
+
     return (
         <>
-        <h1>Episode Details</h1>
+        <h1>{episode?.name}</h1>
+        <h1>EBEM</h1>
+        <audio controls src={episode?.audio}></audio>
         </>
     );
 }
 
-export default detailsEpisode;
+export default DetailsEpisode;

fd13aaa4e7e11ec61f1ba02baaedec957527de93
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-02-05 15:58:21 +0200

    add format duration util

---
+++ client/src/utils/formatDuration.ts
@@ -0,0 +1,7 @@
+export const formatDuration = (durationInSeconds: number) => {
+    const minutes = Math.floor(durationInSeconds / 60);
+    const seconds = Math.floor(durationInSeconds % 60);
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const formattedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${formattedMinutes}:${formattedSeconds}`;
+};

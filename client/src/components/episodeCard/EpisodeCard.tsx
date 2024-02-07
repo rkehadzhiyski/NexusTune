7e258bbcaf9f0c422ddfa23f01766ff47226ba6b
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-02-07 19:43:53 +0200

    create new episode card

--- client/src/components/episodeCard/EpisodeCard.tsx
+++ client/src/components/episodeCard/EpisodeCard.tsx
@@ -1,9 +1,11 @@
-import Card from 'react-bootstrap/Card';
+import { useNavigate } from 'react-router-dom';
 
 import styles from './episodeCard.module.css';
 import { formatDate } from '../../utils/formatDate'
 import { formatDuration } from '../../utils/formatDuration';
-import { useNavigate } from 'react-router-dom';
+
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
 
 interface Episode {
     _id: number;
@@ -26,14 +28,33 @@ const EpisodeCard: React.FC<PodcastCardProps> = ({ episode }) => {
         navigate(`/episode/${episode._id}`);
     };
     return (
-        <Card className={styles['card-container']} onClick={handleClick}>
-            <Card.Header>{episode.name}</Card.Header>
-            <Card.Body>
-                <Card.Img className={styles['card-image']} variant="top" src={episode.image} />
-                <Card.Text>{episode.description}</Card.Text>
-            </Card.Body>
-            <Card.Footer className="text-muted">{formatDuration(episode.duration)} | {formatDate(episode.createdAt)}</Card.Footer>
-        </Card>
+        // <Card className={styles['card-container']} onClick={handleClick}>
+        //     <Card.Header>{episode.name}</Card.Header>
+        //     <Card.Body>
+        //         <Card.Img className={styles['card-image']} variant="top" src={episode.image} />
+        //         <Card.Text>{episode.description}</Card.Text>
+        //     </Card.Body>
+        //     <Card.Footer className="text-muted">{formatDuration(episode.duration)} | {formatDate(episode.createdAt)}</Card.Footer>
+        // </Card>
+
+        <div className={styles['episode-card-container']} >
+            <div className={styles['card-inner-container']} onClick={handleClick}>
+                <div>
+                    <Image className={styles['episode-image']} src={episode.image} rounded />
+                </div>
+                <div className={styles['card-info-container']}>
+                    <div>
+                        <h3>{episode.name}</h3>
+                    </div>
+                    <div className={styles['episode-description']}>
+                        <p>{episode.description}</p>
+                    </div>
+                    <div className={styles['episode-more-info']}>
+                        <p>{formatDuration(episode.duration)} | {formatDate(episode.createdAt)}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 

ea06de4ffcda80f3087a6c9881acc6bf7ffa3771
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-02-09 19:52:09 +0200

    add get latest episode to manager

--- server/managers/episodeManager.js
+++ server/managers/episodeManager.js
@@ -3,3 +3,12 @@ const Episode = require('../models/Episode');
 exports.create = (episodeData) => Episode.create(episodeData);
 
 exports.getOne = (episodeId) => Episode.findById(episodeId);
+
+exports.getLatest = async() =>{
+    try {
+        const episodes = await Episode.find().sort({ createdAt: -1 }).limit(5);
+        return episodes;
+    } catch (error) {
+        throw new Error('Error fetching latest episode');
+    }
+}

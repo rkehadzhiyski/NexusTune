96494e85fd300c2ebb9f16009269b0300f85ecd2
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-02-12 15:17:45 +0200

    make small changes to server

--- server/index.js
+++ server/index.js
@@ -6,22 +6,19 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const { auth } = require('./middlewares/authMiddleware');
 
+const app = express();
+
 const PORT = process.env.PORT || 8888;
 
-const app = express();
+mongoose.set('strictQuery', false);
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('DB connected'))
     .catch(error => console.log(error));
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({
-    origin: ['http://nexus-tune.vercel.app/'],
-    methods: ['POST', 'GET', 'PUT'],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin']
-}));
+app.use(cors());
 app.use(auth);
 
 app.get('/', (req, res) => {
@@ -31,3 +28,10 @@ app.get('/', (req, res) => {
 app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+
+// {
+//     origin: ['http://nexus-tune.vercel.app/'],
+//     methods: ['POST', 'GET', 'PUT'],
+//     credentials: true,
+//     allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin']
+// }

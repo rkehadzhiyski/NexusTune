2063c771a83c1cabfb625a9502caf2ffb01e8926
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-02-16 21:47:07 +0200

    add comments to routes

--- server/routes.js
+++ server/routes.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const userController = require('./controllers/userController');
 const podcastContoller = require('./controllers/podcastController');
 const episodeController = require('./controllers/episodeController');
+const commentController = require('./controllers/commentConroller');
 
 router.use('/users', userController);
 router.use('/podcasts', podcastContoller);
 router.use('/episodes', episodeController);
+router.use('/comments', commentController);
 
 module.exports = router;

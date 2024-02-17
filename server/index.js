b2f6685bf8b1fa01298e965cfd59871a70418923
Radoslav Kehadzkiyski <130061726+rkehadzhiyski@users.noreply.github.com>
2024-02-17 16:30:11 +0200

    add router export

--- server/index.js
+++ server/index.js
@@ -10,8 +10,6 @@ const app = express();
 
 const PORT = process.env.PORT || 8888;
 
-mongoose.set('strictQuery', false);
-
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('DB connected'))
     .catch(error => console.log(error));
